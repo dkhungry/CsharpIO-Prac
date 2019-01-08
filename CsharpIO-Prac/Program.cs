0edf7e323779f075cdc09b46c525ec61403ae59b
dkdev <dahautam@gmail.com>
2019-01-08 05:57:20 -0600

    Added txt file

--- CsharpIO-Prac/Program.cs
+++ CsharpIO-Prac/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace CsharpIO_Prac
 {
@@ -6,7 +7,7 @@ namespace CsharpIO_Prac
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
