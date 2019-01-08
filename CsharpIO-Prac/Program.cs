b830cb2c23e00a709de33c12248977b0d8ce8a3f
dkdev <dahautam@gmail.com>
2019-01-08 06:03:44 -0600

    Added console print of all txt files in current directory

--- CsharpIO-Prac/Program.cs
+++ CsharpIO-Prac/Program.cs
@@ -7,7 +7,14 @@ namespace CsharpIO_Prac
     {
         static void Main(string[] args)
         {
-            
+            string currentDir = Directory.GetCurrentDirectory();
+            DirectoryInfo directory = new DirectoryInfo(currentDir);
+            var files = directory.GetFiles("*.txt");
+
+            foreach(var file in files)
+            {
+                Console.WriteLine(file.Name);
+            }
         }
     }
 }
